423af4ede9d62fa684f4cb28a5a11a286a4c9585
afshin <afshinpht1379@gmail.com>
2025-06-11 01:41:30 +0330

    feat: implement JwtUtil for token generation and validation

---
+++ src/main/java/com/azki/reservation/util/JwtUtil.java
@@ -0,0 +1,49 @@
+package com.azki.reservation.util;
+
+import com.azki.reservation.config.security.JwtProperties;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+@AllArgsConstructor
+public class JwtUtil {
+
+    private final JwtProperties jwtProperties;
+    private final SecretKey secretKey = Keys.hmacShaKeyFor(jwtProperties.getSecret().getBytes());
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtProperties.getAccessTokenValiditySeconds() * 1000))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String getUsernameFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
