61b68e6aca6121198651e322b61ae373bf2d889d
afshin <afshinpht1379@gmail.com>
2025-06-11 23:17:59 +0330

    feat: update Flyway migration to insert 1,000,000 mock slots

--- src/main/resources/db/migration/V3__insert_mock_slots.sql
+++ src/main/resources/db/migration/V3__insert_mock_slots.sql
@@ -3,7 +3,7 @@ DELIMITER $$
 CREATE PROCEDURE insert_large_data()
 BEGIN
     DECLARE i INT DEFAULT 1;
-    WHILE i <= 100000 DO
+    WHILE i <= 1000000 DO
         INSERT INTO available_slots (start_time, end_time, is_reserved) 
         VALUES 
         (current_timestamp + INTERVAL i MINUTE, current_timestamp + INTERVAL (i + 1) MINUTE, FALSE);
