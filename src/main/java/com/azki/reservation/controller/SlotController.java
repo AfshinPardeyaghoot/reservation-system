65372d8a56883f6b03afc58bf4834063f2856bd8
afshinpy <afshinpht1379@gmail.com>
2025-06-11 17:09:12 +0330

    feat: add SlotController for slot endpoints

--- src/main/java/com/azki/reservation/controller/SlotController.java
+++ src/main/java/com/azki/reservation/controller/SlotController.java
@@ -1,4 +1,24 @@
 package com.azki.reservation.controller;
 
+import com.azki.reservation.dto.HttpResponse;
+import com.azki.reservation.dto.SlotDto;
+import com.azki.reservation.service.SlotService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/slots")
+@AllArgsConstructor
 public class SlotController {
+
+    private final SlotService slotService;
+
+    @GetMapping
+    public HttpResponse<Page<SlotDto>> list(Pageable pageable) {
+        return new HttpResponse<>(slotService.getAvailable(pageable));
+    }
 }
