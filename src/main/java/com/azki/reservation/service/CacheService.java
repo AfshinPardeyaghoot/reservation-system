6f04baeb3a5a07f601e423cd6a1a0db159bbfd3d
afshinpy <afshinpht1379@gmail.com>
2025-06-12 10:15:52 +0330

    feat: optimize CacheService to use bulk ZSet addition for Redis storage

--- src/main/java/com/azki/reservation/service/CacheService.java
+++ src/main/java/com/azki/reservation/service/CacheService.java
@@ -6,16 +6,18 @@ import com.azki.reservation.repository.SlotRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.nio.charset.StandardCharsets;
+
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -27,11 +29,13 @@ public class CacheService {
 
     @Transactional(readOnly = true)
     public void populateCache() {
+        redisTemplate.delete(KEY);
+
         int pageSize = 100_000;
         int page = 0;
         boolean hasMore = true;
 
-        while (hasMore) {
+        while (true) {
             Page<Slot> pageResult = slotRepository.findAvailable(PageRequest.of(page, pageSize), LocalDateTime.now());
             List<Slot> available = pageResult.getContent();
             System.out.println("Load available slots to cache (page " + page + "): " + available.size());
@@ -41,18 +45,28 @@ public class CacheService {
                 break;
             }
 
-            redisTemplate.executePipelined((RedisCallback<Object>) cnn -> {
-                available.forEach(s ->
-                        cnn.zAdd(KEY.getBytes(StandardCharsets.UTF_8),
-                                s.getStartTime().toEpochSecond(ZoneOffset.UTC),
-                                s.getId().toString().getBytes(StandardCharsets.UTF_8)));
-                return null;
-            });
+            Set<ZSetOperations.TypedTuple<Object>> tuples = available.stream()
+                    .map(s -> ZSetOperations.TypedTuple.of(
+                            (Object) s.getId().toString(),
+                            (double) s.getStartTime().toEpochSecond(ZoneOffset.UTC)
+                    ))
+                    .collect(Collectors.toSet());
+
+
+            if (!tuples.isEmpty()) {
+                redisTemplate.opsForZSet().add(KEY, tuples);
+            }
+
+            if (!pageResult.hasNext()) {
+                break;
+            }
 
             page++;
         }
+
         System.out.println("Cache population completed.");
     }
+
 
     @Scheduled(cron = "0 0 0 * * ?")
     @Transactional(readOnly = false)
